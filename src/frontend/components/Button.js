25e67064d888bb7ed4429b039d8760e318b8d1d3
OscarEscriba <128449179+OscarEscriba@users.noreply.github.com>
2024-04-18 20:07:09 -0600

    estilo del button.js

--- src/frontend/components/Button.js
+++ src/frontend/components/Button.js
@@ -1,7 +1,15 @@
 import React from 'react'; 
 const Button = ({ onClick, text }) => {
     return (
-        <button onClick={onClick}>
+        <button onClick={onClick}
+        style={{
+            backgroundColor: '#804000',
+            color: 'white', 
+            padding: '10px 20px', 
+            borderRadius: '13px', 
+            border: 'none',
+            cursor: 'pointer', 
+             }}>
         {text}
         </button>
     );
