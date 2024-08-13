62e6e7c1aa351dd954ee133773e33a79e2fee8f4
OscarEscriba <128449179+OscarEscriba@users.noreply.github.com>
2024-08-13 17:26:07 -0600

    pruebaUnitaria de input JEST

---
+++ src/Input.test.js
@@ -0,0 +1,21 @@
+const React = require('react');
+const { render, screen } = require('@testing-library/react');
+require('@testing-library/jest-dom');
+const Input = require('./frontend/components/InputPrueba');
+
+describe('Input Component', () => {
+  test('should render an input with the correct type and placeholder', () => {
+    const type = 'text';
+    const placeholder = 'Enter text';
+
+    render(React.createElement(Input, { type, placeholder }));
+
+    const inputElement = screen.getByPlaceholderText(placeholder);
+
+    expect(inputElement).toBeInTheDocument();
+    expect(inputElement.getAttribute('type')).toBe(type);
+    expect(inputElement.getAttribute('placeholder')).toBe(placeholder);
+  });
+});
+
+
---
+++ src/jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    transform: {},
+    moduleFileExtensions: ['js', 'jsx'],
+    testEnvironment: 'jsdom',
+    transformIgnorePatterns: ['/node_modules/(?!@testing-library)']
+  };
+  
