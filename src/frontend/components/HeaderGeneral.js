a96a9d819fe89f5017fe3191a46cbbb8182f906a
Javier Salazar <sal18764@uvg.edu.gt>
2024-10-06 19:42:01 -0600

    Revert "new cards v1"
    This reverts commit bb923bde40aa74133fb4f1f7bfdc6af447c14839.

--- src/frontend/components/HeaderGeneral.js
+++ src/frontend/components/HeaderGeneral.js
@@ -1,18 +1,51 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../css/Header.css'; 
 
 const Header = ({ isSidebarOpen, toggleSidebar }) => {
-    return (
-        <header className={`header ${isSidebarOpen ? 'header-open' : 'header-closed'}`}>
-            <button className="menu-button" onClick={toggleSidebar}>
-                ☰
-            </button>
-            <Link to='/sessions' className="link">
-                <h1 className="title">SISTEMA TUTO</h1>
-            </Link>
-        </header>
-    );
+  const headerStyle = {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    width: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#9c4922',
+    color: '#fff',
+    padding: isSidebarOpen ? '0 70px' : '0 20px', // Ajusta el padding según el estado del Sidebar
+    transition: 'padding 0.3s ease, height 0.3s ease',
+    height: isSidebarOpen ? '80px' : '60px', // Ajusta la altura según el estado del Sidebar
+  };
+
+  const menuButtonStyle = {
+    position: 'absolute',
+    left: '10px',
+    top: '1px',
+    backgroundColor: 'transparent',
+    border: 'none',
+    color: '#fff',
+    fontSize: '24px',
+    cursor: 'pointer',
+  };
+
+  const titleStyle = {
+    margin: 0,
+    fontSize: '24px',
+    textAlign: 'center',
+    width: '100%',
+    color: '#fff',
+  };
+
+  return (
+    <header style={headerStyle}>
+      <button style={menuButtonStyle} onClick={toggleSidebar}>
+        ☰
+      </button>
+      <Link to='/sessions' className ={titleStyle} style={{textDecoration: 'none'}}>
+      <h1 style={titleStyle}>SISTEMA TUTO</h1>
+      </Link>
+    </header>
+  );
 };
 
 export default Header;
