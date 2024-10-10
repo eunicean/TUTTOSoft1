326a1a57fe58acdba7502f6d8983432aa7804031
Eunice Mata <72279865+eunicean@users.noreply.github.com>
2024-10-10 01:20:42 -0600

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,10 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    "baseUrl": "http://localhost:5173", // Cambia al puerto correcto
     setupNodeEvents(on, config) {
-      // Implementa los event listeners aquí si es necesario
+      // implement node event listeners here
     },
+    
   },
+  
 });
