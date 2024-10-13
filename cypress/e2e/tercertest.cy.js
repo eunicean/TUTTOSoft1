5c0dc048d4b5a157fb4447e3f401af10c94cf58f
EJGDLG <edwinjosegabriel@gmail.com>
2024-10-12 19:18:38 -0600

    update

--- cypress/e2e/tercertest.cy.js
+++ cypress/e2e/tercertest.cy.js
@@ -23,25 +23,4 @@ describe('Prueba de Regresión: Actualización de Perfil', () => {
   it('Debería mostrar la página de perfil correctamente', () => {
     cy.url().should('include', '/profile');
   });
-
-
-  it('Debería permitir cerrar la sesión desde la barra lateral', () => {
-    cy.get('.menu-toggle').click();
-    cy.get('li').contains('Cerrar Sesión').click({ force: true });
-    cy.url().should('include', '/login');
-  });
-
-  it('Debería permitir editar y guardar los cambios en el perfil', () => {
-    cy.get('input[name="username"]').clear().type('NuevoUsuario');
-    cy.get('button').contains('Guardar').click();
-    
-    // Verificar que se muestre la notificación de éxito
-    cy.get('.notification').should('contain', 'Perfil actualizado');
-    
-    // Verificar que el nombre de usuario se actualizó en la interfaz
-    cy.get('.user-name').should('contain', 'NuevoUsuario');
-
-    // Recargar la página para verificar que el cambio persiste
-    cy.reload();
-  });
 });
