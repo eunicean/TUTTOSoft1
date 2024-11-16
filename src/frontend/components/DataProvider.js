2e2f1c20225ed3d811ff0e2b7f19fbbb1bf0f1a4
Eunice Mata <eunice.mata2992@gmail.com>
2024-11-15 18:56:56 -0600

    data provider updated

--- src/frontend/components/DataProvider.js
+++ src/frontend/components/DataProvider.js
@@ -6,16 +6,11 @@ export const DataProvider = ({ children }) => {
   const [users, setUsers] = useState([
     { name: 'tuto test', role: 'tutor', subjects: ['Física I', 'Historia Universal',' Matemáticas Básicas',' Programación I'], year: 2, rating: 4 },
     { name: 'tutotes2', role: 'tutor', subjects: [' Matemáticas Básicas'], year: 3, rating: 5 },
-    { name: 'Marta Hernández', role: 'estudiante', subjects: [], year: 2, rating: 3 },
-    { name: 'Luis Gómez', role: 'estudiante', subjects: [], year: 4, rating: 2 },
-    { name: 'María Díaz', role: 'estudiante', subjects: [], year: 3, rating: 5 },
-    { name: 'Ricardo Sánchez', role: 'estudiante', subjects: [], year: 1, rating: 4 },
+    { name: 'Marta Hernández', role: 'estudiante', subjects: [], year: 2, rating: 0 },
+    { name: 'Luis Gómez', role: 'estudiante', subjects: [], year: 4, rating: 0 },
+    { name: 'María Díaz', role: 'estudiante', subjects: [], year: 3, rating: 0 },
+    { name: 'Ricardo Sánchez', role: 'estudiante', subjects: [], year: 1, rating: 0 },
     { name: 'Carlos García', role: 'estudiante', subjects: [], year: 1, rating: 0 },
-    { name: 'Sofía Martínez', role: 'estudiante', subjects: [], year: 4, rating: 0 },
-    { name: 'Lucía Gómez', role: 'estudiante', subjects: [], year: 2, rating: 0 },
-    { name: 'Pedro Fernández', role: 'estudiante', subjects: [], year: 3, rating: 0 },
-    { name: 'Jorge Ruiz', role: 'estudiante', subjects: [], year: 2, rating: 0 },
-    { name: 'Elena Torres', role: 'estudiante', subjects: [], year: 1, rating: 0 },
   ]);
 
   const toggleUserRole = (index) => {
