e70c87a74969e0788b0f68610063c560ec21e18b
Eunice Mata <eunice.mata2992@gmail.com>
2024-11-15 18:51:34 -0600

    data provider updated

--- src/frontend/components/DataProvider.js
+++ src/frontend/components/DataProvider.js
@@ -4,12 +4,12 @@ const DataContext = createContext();
 
 export const DataProvider = ({ children }) => {
   const [users, setUsers] = useState([
-    { name: 'Juan Pérez', role: 'tutor', subjects: ['Física I', 'Cálculo II'], year: 2, rating: 4 },
-    { name: 'Ana López', role: 'tutor', subjects: ['Mate discreta', 'Teoría de probabilidades'], year: 3, rating: 5 },
-    { name: 'Marta Hernández', role: 'tutor', subjects: ['Historia Universal'], year: 2, rating: 3 },
-    { name: 'Luis Gómez', role: 'tutor', subjects: ['Cálculo I', 'Pensamiento cuantitativo'], year: 4, rating: 2 },
-    { name: 'María Díaz', role: 'tutor', subjects: ['Cálculo II', 'Teoría de probabilidades'], year: 3, rating: 5 },
-    { name: 'Ricardo Sánchez', role: 'tutor', subjects: ['Matemáticas Básicas'], year: 1, rating: 4 },
+    { name: 'tuto test', role: 'tutor', subjects: ['Física I', 'Historia Universal',' Matemáticas Básicas',' Programación I'], year: 2, rating: 4 },
+    { name: 'tutotes2', role: 'tutor', subjects: [' Matemáticas Básicas'], year: 3, rating: 5 },
+    { name: 'Marta Hernández', role: 'estudiante', subjects: [], year: 2, rating: 3 },
+    { name: 'Luis Gómez', role: 'estudiante', subjects: [], year: 4, rating: 2 },
+    { name: 'María Díaz', role: 'estudiante', subjects: [], year: 3, rating: 5 },
+    { name: 'Ricardo Sánchez', role: 'estudiante', subjects: [], year: 1, rating: 4 },
     { name: 'Carlos García', role: 'estudiante', subjects: [], year: 1, rating: 0 },
     { name: 'Sofía Martínez', role: 'estudiante', subjects: [], year: 4, rating: 0 },
     { name: 'Lucía Gómez', role: 'estudiante', subjects: [], year: 2, rating: 0 },
